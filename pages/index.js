08564cf7f960427224f19de24087d1c354f8817f
Albin Ekblom <albinekblom@gmail.com>
2018-03-20 14:30:37 +0100

    🔥 Replace modern-normalize with eightpoint

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import 'modern-normalize/modern-normalize.css'
+import 'eightpoint/dist/eight.min.css'
 
 import Hero from '../components/sections/hero'
 import Video from '../components/sections/video'
