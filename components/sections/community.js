1851e8acc129ecba69130811172cc135c167554a
Albin Ekblom <albinekblom@gmail.com>
2018-03-21 21:27:30 +0100

    🎉 Link all logos

--- components/sections/community.js
+++ components/sections/community.js
@@ -1,13 +1,18 @@
 import Section from '../section'
 
-const Logo = ({ href, name }) => <img key={name} src={`/static/images/logos/${name}.png`} />
+const Logo = ({ href, name }) => (
+  <a href={href} title={name}>
+    <img key={name} src={`/static/images/logos/${name}.png`} />
+  </a>
+)
 const Logos = () => (
   <div>
-    <Logo name="zeit" />
-    <Logo name="circle-ci" />
-    <Logo name="git-hub" />
-    <Logo name="google" />
-    <Logo name="facebook" />
+    <Logo name="zeit" href="https://zeit.co/" />
+    <Logo name="circle-ci" href="https://circleci.com/" />
+    <Logo name="sentry" href="https://getsentry.io/" />
+    <Logo name="git-hub" href="https://github.com/" />
+    <Logo name="google" href="https://google.com/" />
+    <Logo name="facebook" href="https://facebook.com/" />
   </div>
 )
 export default () => (
