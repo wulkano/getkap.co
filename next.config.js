71b94ba4ae944aa5a17cb0896f6688ec40f53303
Albin Ekblom <albinekblom@gmail.com>
2018-03-22 17:52:06 +0100

    🎉 Use static export

--- next.config.js
+++ next.config.js
@@ -2,12 +2,23 @@ const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withPlugins([
-  [withCSS, {}],
-  [
-    optimizedImages,
-    {
-      optimizeImagesInDev: true
+const nextConfig = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
     }
-  ]
-])
+  }
+}
+
+module.exports = withPlugins(
+  [
+    [withCSS, {}],
+    [
+      optimizedImages,
+      {
+        optimizeImagesInDev: true
+      }
+    ]
+  ],
+  nextConfig
+)
