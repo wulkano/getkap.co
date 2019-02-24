ab77ab42a9e23c2cb8c0974196588cea4a5dcf3d
timothyis <hello@timothy.is>
2019-02-24 18:07:25 +0000

    Add Now deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const optimizedImages = require('next-optimized-images')
 const withCSS = require('@zeit/next-css')
 
 const nextConfig = {
+  target: 'serverless',
   exportPathMap: function() {
     return {
       '/': { page: '/' }
