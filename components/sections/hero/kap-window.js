ec265010bbc288333a57b80a063eb3a015fe81bc
Albin Ekblom <albinekblom@gmail.com>
2019-09-16 23:02:09 +0200

    Update kap window

--- components/sections/hero/kap-window.js
+++ components/sections/hero/kap-window.js
@@ -3,10 +3,10 @@ import screenSizes from '../../../lib/screen-sizes'
 
 const KapWindow = () => (
   <div className="window">
-    <img src={require('../../../static/images/kap-3@2x.png?inline')} alt="The Kap Window" />
+    <img src={require('../../../static/images/app-light.png?inline')} alt="The Kap Window" />
     <style jsx>{`
       img {
-        max-width: 464px;
+        max-width: 466px;
         height: auto;
         opacity: 1;
         user-select: none;
@@ -15,13 +15,13 @@ const KapWindow = () => (
 
       .window {
         z-index: 100;
-        max-width: 464px;
-        max-height: 64px;
+        max-width: 466px;
+        max-height: 66px;
         width: 100%;
         height: auto;
         box-shadow: 0 20px 40px 0 rgba(0, 0, 0, 0.1), 0 0 1px rgba(0, 0, 0, 0.4);
         background-color: white;
-        border-radius: 4px;
+        border-radius: 5px;
         position: relative;
         margin: 0 32px;
         margin-bottom: -32px;
