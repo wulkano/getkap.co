5285438693d880475a15b332dac4c068555dd675
George Karagkiaouris <karaggeorge0@gmail.com>
2022-01-18 06:56:52 +0200

    Adjust logic for two artifacts (#46)

--- components/sections/hero/index.js
+++ components/sections/hero/index.js
@@ -1,20 +1,47 @@
+import * as Popover from '@radix-ui/react-popover';
 import Button from '../../Button'
 import KapWindow from './kap-window'
 import Info from './info'
 import Logo from '../../../public/static/images/kap.svg'
-import DownloadIcon from '../../../public/static/images/download.svg'
+import ExpandMoreIcon from '../../../public/static/images/expand-more.svg'
 import ReactGA from 'react-ga'
 import styles from './hero.module.css'
+import buttonStyles from '../../Button/Button.module.css';
+
 
 const Hero = () => (
   <section className={styles.hero}>
     <header className={`${styles.header} grid`}>
       <Logo />
       {/* Download Button */}
-      <Button theme="light" onClick={() => ReactGA.pageview('/download')} href="/api/download" download>
-        <DownloadIcon />
-        <span className={styles.buttonText}>Get kap</span>
-      </Button>
+      <Popover.Root>
+        <Popover.Trigger className={`${buttonStyles.button} ${buttonStyles.light} ${styles.downloadButton}`}>
+          <span className={styles.buttonText}>Get kap</span>
+          <ExpandMoreIcon />
+        </Popover.Trigger>
+        <Popover.Content className={styles.popoverContent} sideOffset={5} align='end'>
+          <ul>
+            <li>
+              <a
+                onClick={() => ReactGA.pageview('/download/arm64')}
+                href="/api/download/arm64"
+                download
+              >
+                Download for Apple (M1)
+              </a>
+            </li>
+            <li>
+            <a
+                onClick={() => ReactGA.pageview('/download/x64')}
+                href="/api/download/x64"
+                download
+              >
+                Download for Intel
+              </a>
+            </li>
+          </ul>
+        </Popover.Content>
+      </Popover.Root>
     </header>
 
     <div className={styles.heroContent}>
